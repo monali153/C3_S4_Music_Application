70def93f4048a1bd9f8d8ef1247e174c1f3fdd9a
Monali Sharma <mona.chaudhari444@gmail.com>
2022-11-23 15:11:03 +0530

    mc complete

---
+++ UserTrackService/src/main/java/com/example/challenge/UserTrackService/exception/UserNotFoundException.java
@@ -0,0 +1,2 @@
+package com.example.challenge.UserTrackService.exception;public class UserNotFoundException {
+}
---
+++ UserTrackService/src/main/java/com/example/challenge/UserTrackService/repository/UserRepository.java
@@ -0,0 +1,2 @@
+package com.example.challenge.UserTrackService.repository;public class UserRepository {
+}
